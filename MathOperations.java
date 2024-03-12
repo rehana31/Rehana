762b42294fe5919cc92ac0ad75e9948cd39e3497
rehana31 <163115847+rehana31@users.noreply.github.com>
2024-03-12 14:36:44 +0530

    Add files via upload

---
+++ MathOperations.java
@@ -0,0 +1,27 @@
+
+// 2)Create a Java class MathOperations with overloaded methods for addition (add) that can accept two integers, three doubles, and two strings, respectively. Demonstrate the usage of these methods in the main method by adding two integers, three doubles, and concatenating two strings.
+
+public class MathOperations{
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public double add(double a, double b, double c){
+        return a + b + c;
+    }
+    
+    public String add(String a, String b){
+        return a + b;
+    }
+
+    public static void main(String[] args){
+        MathOperations op = new MathOperations();
+        int IntSum = op.add(35, 87);
+        System.out.println("Sum of two integers: " + IntSum);
+        double DoubleSum = op.add(91.02, 56.91, 11.11);
+        System.out.println("Sum of three doubles: " + DoubleSum);
+        String StringConcat = op.add("Welcome", "to India!");
+        System.out.println("Concatenation of two strings: " + StringConcat);
+    }
+}
